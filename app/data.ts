6539e04baf0805ebc16fd737f3d5f169b1483a8e
OffshoreResourcing Content Agent <content-agent@offshoreresourcing.com>
2026-09-24 14:14:16 +0000

    Publish September 24 offshore staffing guides

--- app/data.ts
+++ app/data.ts
@@ -15,6 +15,7 @@ import { dailyBlogSep10Posts } from './daily-blog-2026-09-10';
 import { dailyBlogSep18Offa60Posts } from './daily-blog-2026-09-18-offa-60';
 import { dailyBlogSep22Posts } from './daily-blog-2026-09-22';
 import { dailyBlogSep22Offa64Posts } from './daily-blog-2026-09-22-offa-64';
+import { dailyBlogSep24Posts } from './daily-blog-2026-09-24';
 
 export const site = {
   "domain": "OffshoreResourcing.com",
@@ -255,7 +256,7 @@ const augustTenBlogDates: Record<string, string> = {
 
 type BlogPost = { slug: string; title: string; excerpt: string; minutes: number; publishedAt?: string };
 
-export const blogPosts: readonly BlogPost[] = [...dailyBlogSep22Offa64Posts, ...dailyBlogSep22Posts, ...dailyBlogSep18Offa60Posts, ...(blogPostsSource as readonly BlogPost[]), ...dailyBlogPosts, ...dailyBlog20Posts, ...repairBlog21Posts, ...sourceRepair2Posts, ...dailyBlog23Posts, ...august23BlogPosts, ...dailyBlog31Posts, ...dailyBlogSep1Posts, ...dailyBlogSep2Posts, ...dailyBlogSep4Posts, ...dailyBlogSep7Posts, ...dailyBlogSep8Posts, ...dailyBlogSep9Posts, ...dailyBlogSep10Posts]
+export const blogPosts: readonly BlogPost[] = [...dailyBlogSep24Posts, ...dailyBlogSep22Offa64Posts, ...dailyBlogSep22Posts, ...dailyBlogSep18Offa60Posts, ...(blogPostsSource as readonly BlogPost[]), ...dailyBlogPosts, ...dailyBlog20Posts, ...repairBlog21Posts, ...sourceRepair2Posts, ...dailyBlog23Posts, ...august23BlogPosts, ...dailyBlog31Posts, ...dailyBlogSep1Posts, ...dailyBlogSep2Posts, ...dailyBlogSep4Posts, ...dailyBlogSep7Posts, ...dailyBlogSep8Posts, ...dailyBlogSep9Posts, ...dailyBlogSep10Posts]
   .map((post): BlogPost => augustTenBlogDates[post.slug] ? { ...post, publishedAt: augustTenBlogDates[post.slug] } : post)
   .sort((a, b) => (b.publishedAt || '').localeCompare(a.publishedAt || '') || (blogPostsSource.findIndex((item) => item.slug === a.slug) - blogPostsSource.findIndex((item) => item.slug === b.slug)));
 
